4df21326f4e2e5c2ce428ea44221c351cc45861f
Bekjan0292 <166012227+Bekjan0292@users.noreply.github.com>
2024-11-17 23:04:19 +0900

    Update 5_About.py

--- pages/5_About.py
+++ pages/5_About.py
@@ -1,3 +1,19 @@
 import streamlit as st
 
-st.title("What is Technical Analysis?")
+# Title for the About Page
+st.title("About This Application")
+
+# About Message
+st.markdown("""
+This application was prepared for the course **"Investments and Machine Learning"** in 2024.
+
+It is designed to assist users in analyzing stocks using technical and fundamental approaches, 
+providing valuable insights for both short-term and long-term investment strategies.
+
+Thank you for using this application. We hope it enhances your investment journey!
+Nurbek, Baratov
+Gulrukh, Rakhimova
+Temur, Jurabekov
+Gulnar, Yermagambetova
+Bekjan, Jyrgalbek uulu
+""")
